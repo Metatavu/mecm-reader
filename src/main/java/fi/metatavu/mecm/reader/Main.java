3d3eb801f68864a8cdceac4e4a54c763beec9066
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-22 07:05:15 +0200

    Added usage

--- src/main/java/fi/metatavu/mecm/reader/Main.java
+++ src/main/java/fi/metatavu/mecm/reader/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -25,22 +27,17 @@ public class Main {
   private static final String OPTION_OUTPUT = "output";
   private static final String OPTION_OUTPUT_FORMAT = "output-format";
   private static final String OPTION_INPUT = "input";
+  private static final String OPTION_HELP = "help";
   
   private Main() {
   }
   
   @SuppressWarnings ("squid:S1301")
   public static void main(String[] args) throws ParseException {
-    Options options = new Options();
-    
-    options.addOption(createOption(true, "i", OPTION_INPUT, true, "Input file name (MECM)"));
-    options.addOption(createOption(true, "f", OPTION_OUTPUT_FORMAT, true, String.format("Format the input file will be converted. Poosible values: %s", StringUtils.join(OutputFormat.values(), ","))));
-    options.addOption(createOption(true, "o", OPTION_OUTPUT, true, "Output file name)"));
-    options.addOption(createOption(true, "O", OPTION_ORGANIZATION, true, "Organization Id"));
-    options.addOption(createOption(true, "t", OPTION_URI_TEMPLATE, false, "URI Template. Use %s as placeholder for id"));
-    
-    CommandLineParser parser = new DefaultParser();
-    CommandLine commandLine = parser.parse(options, args);
+    CommandLine commandLine = handleOptions(args);
+    if (commandLine == null) {
+      return;
+    }
     
     OutputFormat outputFormat = EnumUtils.getEnum(OutputFormat.class, commandLine.getOptionValue(OPTION_OUTPUT_FORMAT));
     File inputFile = new File(commandLine.getOptionValue(OPTION_INPUT));
@@ -61,6 +58,38 @@ public class Main {
       break;
       default:
     }
+  }
+
+  @SuppressWarnings ({"squid:S1166"})
+  private static CommandLine handleOptions(String[] args) throws ParseException {
+    Options options = new Options();
+    
+    options.addOption(createOption(true, "i", OPTION_INPUT, true, "Input file name (MECM)"));
+    options.addOption(createOption(true, "f", OPTION_OUTPUT_FORMAT, true, String.format("Format the input file will be converted. Poosible values: %s", StringUtils.join(OutputFormat.values(), ","))));
+    options.addOption(createOption(true, "o", OPTION_OUTPUT, true, "Output file name)"));
+    options.addOption(createOption(true, "O", OPTION_ORGANIZATION, true, "Organization Id"));
+    options.addOption(createOption(false, "t", OPTION_URI_TEMPLATE, true, "URI Template. Use %s as placeholder for id"));
+    options.addOption(createOption(false, "h", OPTION_HELP, false, "Prints help"));
+    
+    CommandLineParser parser = new DefaultParser();
+    try {
+      CommandLine commandLine = parser.parse(options, args);
+      if (commandLine.hasOption(OPTION_HELP)) {
+        printHelp(options);
+        return null;
+      }
+      
+      return commandLine;
+    } catch (MissingOptionException e) {
+      printHelp(options);
+    }
+    
+    return null;
+  }
+
+  private static void printHelp(Options options) {
+    HelpFormatter helpFormatter = new HelpFormatter();
+    helpFormatter.printHelp("java -jar mecm-reader.jar", options);
   }
 
   @SuppressWarnings ({"squid:S106", "squid:S1166"})
