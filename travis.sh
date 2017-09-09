386133c05fae106d1b72fda67d10de12bccedb0c
Antti Leppä <antti.leppa@metatavu.fi>
2017-09-09 14:30:42 +0300

    Travis

--- travis.sh
+++ travis.sh
@@ -4,26 +4,32 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${
   echo "Pull request"
   
   PROJECT_VERSION=`mvn -f pom.xml -q -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
-  
-  sh sonar-scanner/bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.analysis.mode=issues \
-    -Dsonar.login=$SONAR_TOKEN \
-    -Dsonar.projectKey=$SONAR_PROJECT_KEY \
-    -Dsonar.projectName=mecm-reader \
-    -Dsonar.projectVersion=$PROJECT_VERSION \
-    -Dsonar.sources=src \
-    -Dsonar.java.source=1.8 \
-    -Dsonar.github.oauth=$GITHUB_TOKEN \
-    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
-  set -e
   mvn clean test -DrepoToken=$COVERALLS_TOKEN -DskipCoverage=false
-  set +e
+  TEST_STATUS=$?
+  
+  if [ "$TEST_STATUS" != "0" ]; then
+    echo "Build failed"
+  else
+    sh sonar-scanner/bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+      -Dsonar.projectName=mecm-reader \
+      -Dsonar.projectVersion=$PROJECT_VERSION \
+      -Dsonar.sources=src \
+      -Dsonar.java.binaries=target/classes \
+      -Dsonar.java.source=1.8 \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+  fi
+  
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_BRANCH == "develop" ]; then
 
   echo "Develop build"
   
   PROJECT_VERSION=`mvn -f pom.xml -q -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
+  mvn clean install
   
   sh sonar-scanner/bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.analysis.mode=publish \
@@ -32,6 +38,7 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_BRANCH == "develop" ]; t
     -Dsonar.projectName=mecm-reader \
     -Dsonar.projectVersion=$PROJECT_VERSION \
     -Dsonar.sources=src \
+    -Dsonar.java.binaries=target/classes \
     -Dsonar.java.source=1.8
 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
